c9609120081f820107b533f3e4e41f5d65a55e05
Abram Hindle <abram.hindle@softwareprocess.es>
2014-11-23 11:35:46 -0700

    XARGS -> xargs
    Former-commit-id: 9c1f36e535f15e275de7610e24953d295da038d6

--- data/summarize.sh
+++ data/summarize.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm runs.csv
-find -name 'large.json' | sed -e 's/\/large.json//' | XARGS -i echo '( cd {} ; python3 ../../dumpbayes.py ; cd .. )' | bash | tee runs.csv
+find -name 'large.json' | sed -e 's/\/large.json//' | xargs -i echo '( cd {} ; python3 ../../dumpbayes.py ; cd .. )' | bash | tee runs.csv
 bash means.sh
